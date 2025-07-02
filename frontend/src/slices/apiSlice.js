6571bf83d4ad1cc3fb57ff856face1acc4084644
ebsonjoy <ebsonjoy721@gmail.com>
2025-07-02 12:45:12 +0530

    fixed baseUrl

--- frontend/src/slices/apiSlice.js
+++ frontend/src/slices/apiSlice.js
@@ -1,7 +1,7 @@
 import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react';
 // const baseQuery = fetchBaseQuery({baseUrl : ''});
 const baseQuery = fetchBaseQuery({
-  baseUrl: 'https://mern-auth-066k.onrender.com/api',
+  baseUrl: 'https://mern-auth-066k.onrender.com',
   credentials: 'include',
 });
 
