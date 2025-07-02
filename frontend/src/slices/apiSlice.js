6eaa293ed51425332b0a0a02966740a19dcfa68d
ebsonjoy <ebsonjoy721@gmail.com>
2025-07-02 12:25:19 +0530

    fixed frontend baseUrl

--- frontend/src/slices/apiSlice.js
+++ frontend/src/slices/apiSlice.js
@@ -1,5 +1,10 @@
 import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react';
-const baseQuery = fetchBaseQuery({baseUrl : ''});
+// const baseQuery = fetchBaseQuery({baseUrl : ''});
+const baseQuery = fetchBaseQuery({
+  baseUrl: 'https://mern-auth-066k.onrender.com/api',
+  credentials: 'include',
+});
+
 export const apiSlice = createApi({
     baseQuery,
     tagTypes : ['User'],
